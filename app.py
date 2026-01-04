18bf6611b5cd0274e268458a9430e91df2a56856
MAAZ <m69023326@gmail.com>
2026-01-04 13:28:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown("""
     /* Background Setup */
     .stApp {
         background: linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), 
-                    url('https://images.unsplash.com/photo-1563013544-824ae1b704d3?q=80&w=2070');
+                    url('https://wise.com/imaginary-v2/d9d50c9d096e6bb7490b1e6a72f65f83.jpg?width=1200');
         background-size: cover;
         background-attachment: fixed;
     }
@@ -148,3 +148,4 @@ with col_kpi:
 
 st.markdown('</div>', unsafe_allow_html=True)
 st.caption("© 2026 FraudGuard Global Security | Secure Data Processing Unit")
+
